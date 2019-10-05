ba201cffa97e1da71c728634ebdafa51a9e05b49
wyom1ng <edhimmel@gmx.net>
2019-10-05 23:50:54 +0200

    added scrolling

--- js/main.js
+++ js/main.js
@@ -1,23 +1,42 @@
 document.addEventListener('DOMContentLoaded', evt => {
     const layers = document.querySelectorAll('.parallax');
+    const main = document.getElementById('main');
+    const maxY = 0;
+    const minY = -(10000 - window.innerHeight); // negative (pageheight - viewport height)
 
-    const onDrag = () => {
+    const transformElement = (element, pos) => {
+        element.setAttribute('style', `transform: translate3d(0px, ${pos}px, 0px)`);
+    };
+    const tweenElement = (element, pos) => {
+        TweenLite.to(element, 0.3, {top: pos});
+    };
+
+    const onDrag = moveElementFn => {
+        moveElementFn = moveElementFn instanceof Function ? moveElementFn : transformElement;
         for (let i = 0; i < layers.length; i++) {
             const layer = layers[i];
             const speed = parseInt(layer.getAttribute('data-speed'), 10) / 100;
-            const yPos = draggable.y * speed + 1;
-            layer.setAttribute('style', `transform: translate3d(0px, ${yPos}px, 0px)`);
+            const yPos = draggable.y * speed + 1;   // offset by one pixel to bypass transform glitches (white lines)
+            moveElementFn(layer, yPos);
         }
     };
 
-    const draggable = Draggable.create('#main', {
+    const draggable = Draggable.create(main, {
         allowContextMenu: true,
         type: 'y',
-        bounds: { maxY: 0, minY: -(10000 - window.innerHeight)},
+        bounds: { maxY, minY },
         cursor: 'default',
         throwProps: true,
         onDrag,
         onThrowUpdate: onDrag,
         zIndexBoost: false
     })[0];
+
+    document.addEventListener('wheel', evt => {
+        draggable.y -= evt.deltaY;
+        if (draggable.y > maxY) draggable.y = maxY;
+        if (draggable.y < minY) draggable.y = minY;
+        tweenElement(main, draggable.y);
+        onDrag(tweenElement);
+    })
 });
